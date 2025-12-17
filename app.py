e7386cd0044d079c5379f52e4e0d785d47c98de4
dongpradip <st20341085@outlook.cardiffmet.ac.uk>
2025-12-17 20:19:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,8 @@ import aqi_predcition   # fixed spelling
 # Load the dataframe
 df = pd.read_csv("cleaned_air_quality.csv")
 
-# st.sidebar.markdown("<h2 style='text-align:center; color:green;'>India Air Quality</h2>", unsafe_allow_html=True)
-# st.sidebar.markdown("---")
-# page = st.sidebar.radio("Choose Page", ["Dataset Overview", "EDA", "AQI Prediction"])
-# st.sidebar.markdown("---")
 st.sidebar.markdown(
-    "<h2 style='text-align:center; color:green;'>🇮🇳 India Air Quality</h2>",
+    "<h2 style='text-align:center; color:green;'>India Air Quality Analysis</h2>",
     unsafe_allow_html=True
 )
 st.sidebar.markdown("---")
@@ -28,13 +24,6 @@ app = MultiApp()
 app.add_app("Dataset Overview", lambda: dataset_overview.app(df))
 app.add_app("EDA", lambda: eda.app(df))
 app.add_app("AQI Prediction", lambda: aqi_predcition.app())
-
-# if page == "Dataset Overview":
-#     dataset_overview.app(df)
-# elif page == "EDA":
-#     eda.app(df)
-# elif page == "AQI Prediction":
-#     aqi_predcition.app()
 
 # Run the app
 app.run()
